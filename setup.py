e2a91fb414c625ce0e74ebb0033a5af5a1af130e
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2020-12-07 17:38:55 +0100

    removed python 3.5 and switched to github actions

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
